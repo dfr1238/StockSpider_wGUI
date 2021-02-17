64e2e04ccee3cadb3ea32a4224837d7c88ef7325
dfr1238 <dfr1238@gmail.com>
2021-02-17 18:54:42 +0800

    初始GUI

--- stock_gui.py
+++ stock_gui.py
@@ -1,12 +1,33 @@
 import PySimpleGUI as sg
+from datetime import datetime
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+sg.theme('DarkAmber') #設定顏色主題
 
-window = sg.Window("Demo", layout, margins=(200,100))
+this_Year = datetime.today().year #獲取今年年份
+year_list =[] #存放年份
+season_list =[1,2,3,4] #存放季度
+for i in range(2000,this_Year+1): #新增從2000至今年的年份至列表中
+    year_list.append('%5d' % i)
 
+
+#主視窗設計
+main_Layout = [ [sg.Text('（自動＼手動）抓取設定')],
+                [sg.DropDown(year_list, size=(6,5)),sg.Text('年'),sg.DropDown(season_list, size=(2,5)),sg.Text('季度')],
+                [sg.Text('自動抓取資料'),sg.Button('自動爬取')],
+                [sg.Text('手動輸入股號抓取資料')],
+                [sg.InputText(size=(10,5)),sg.Button('單筆抓取')],
+                [sg.Text('運行計算式')],
+                [sg.Button('公式一'),sg.Button('公式二'),sg.Button('公式三'),sg.Button('公式四')],
+                [sg.Text('其他選項')],
+                [sg.Button('編輯股號表'),sg.Button('設定'),sg.Button('關閉')] ]
+
+#創建視窗
+main_Window = sg.Window("股票資料抓取與運算", main_Layout, margins=(40,20))
+
+#從視窗讀取資料
 while True:
-    event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+    event, values = main_Window.read()
+    if event == "關閉" or event == sg.WIN_CLOSED:
         break
 
-window.close()
+main_Window.close()
