16a526ca897e4252880cc51fc54fa1a05ec0c0c2
dfr1238 <dfr1238@gmail.com>
2021-02-21 06:56:41 +0800

    停用telent

--- StockScrapyProject/StockScrapyProject/settings.py
+++ StockScrapyProject/StockScrapyProject/settings.py
@@ -59,9 +59,9 @@ RETRY_TIMES = 15
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+EXTENSIONS = {
+    'scrapy.extensions.telnet.TelnetConsole': None,
+}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
