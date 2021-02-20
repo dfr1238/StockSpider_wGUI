3ee1468d98674f368420688d72910a3f468a349f
dfr1238 <dfr1238@gmail.com>
2021-02-21 06:56:52 +0800

    修改爬蟲運作的過程

--- StockScrapyProject/StockScrapyProject/run_scraper.py
+++ StockScrapyProject/StockScrapyProject/run_scraper.py
@@ -1,7 +1,10 @@
 from StockScrapyProject.StockScrapyProject.spiders.stockSpider import StockSpider
 from StockScrapyProject.StockScrapyProject.spiders.stockPriceSpider import stockPriceSpider
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+from twisted.internet import reactor
+from twisted.internet import defer
 import os
 
 
@@ -11,7 +14,14 @@ class Scraper:
         os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
         self.process = CrawlerProcess(get_project_settings())
         self.spider = StockSpider # The spider you want to crawl
-
-    def run_StockSpider(self,Year='',Season='',CSV='',Mode='',CO_ID='', **kwargs):
-        self.process.crawl(self.spider,Year=Year,Season=Season,CSV=CSV,Mode=Mode,CO_ID=CO_ID)
-        self.process.start()  # the script will block here until the crawling is finishe
+        self.runner  = CrawlerRunner(get_project_settings())
+    dfs = set()
+    def set_StockSpider(self,Year='',Season='',CSV='',Mode='',CO_ID='', **kwargs):
+        runner  = CrawlerRunner(get_project_settings())
+        d = runner.crawl(self.spider,Year=Year,Season=Season,CSV=CSV,Mode=Mode,CO_ID=CO_ID)
+        self.dfs.add(d)
+        defer.DeferredList(self.dfs).addBoth(lambda _: reactor.stop())
+        #d.addBoth(lambda _: reactor.stop())
+    
+    def run_StockSpider(self):
+        reactor.run() # the script will block here until the crawling is finished
