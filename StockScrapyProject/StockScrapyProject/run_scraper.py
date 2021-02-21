59b06b94d42e46841f59af57b5de2c7fd2875d75
dfr1238 <dfr1238@gmail.com>
2021-02-22 06:21:36 +0800

    傳遞用戶自訂的配置給於爬蟲

--- StockScrapyProject/StockScrapyProject/run_scraper.py
+++ StockScrapyProject/StockScrapyProject/run_scraper.py
@@ -3,24 +3,36 @@ from StockScrapyProject.StockScrapyProject.spiders.stockPriceSpider import stock
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from scrapy.utils.project import get_project_settings
 import os
-
+from twisted.internet import reactor
+from twisted.names import client
+from multiprocessing import Process, Queue
 
 class Scraper:
     def __init__(self):
         settings_file_path = 'StockScrapyProject.StockScrapyProject.settings' # The path seen from root, ie. from main.py
         os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
-        self.process = CrawlerProcess(get_project_settings())
-        self.spider = StockSpider # The spider you want to crawl
-        self.runner  = CrawlerRunner(get_project_settings())
+        setting = get_project_settings()
+        self.process = CrawlerProcess(setting)
+        self.runner  = CrawlerRunner(setting)
+
+    def change_Project_Setting(self,MONOGBURL,DBNAME,CONAME):
+        setting = get_project_settings()
+        setting.set('MONGO_URI',MONOGBURL)
+        setting.set('MONGO_DATABASE',DBNAME)
+        setting.set('MONGO_CODATA',CONAME)
+        self.process = CrawlerProcess(setting)
+        self.runner  = CrawlerRunner(setting)
 
     def set_PriceSpider(self,CSV=''):
-        self.process.crawl(stockPriceSpider,CSV_File_PATH=CSV)
+        d = self.runner.crawl(stockPriceSpider,CSV_File_PATH=CSV)
+        d.addBoth(lambda _: reactor.stop())
     
     def run_PriceSpider(self):
-        self.process.start()
+        reactor.run()
 
     def set_StockSpider(self,Year='',Season='',CSV='',Mode='',CO_ID='', **kwargs):
-        self.process.crawl(StockSpider,Year=Year,Season=Season,CSV=CSV,Mode=Mode,CO_ID=CO_ID)
+        d = self.runner.crawl(StockSpider,Year=Year,Season=Season,CSV=CSV,Mode=Mode,CO_ID=CO_ID)
+        d.addBoth(lambda _: reactor.stop())
     
     def run_StockSpider(self):
-        self.process.start()
+        reactor.run()
