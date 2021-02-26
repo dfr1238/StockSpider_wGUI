f2bc4ab2d9d6b2041113b9173d5752b23c656927
dfr1238 <dfr1238@gmail.com>
2021-02-26 20:05:15 +0800

    加入了不同欄位的相容性 增加手動取消機制

--- StockScrapyProject/StockScrapyProject/spiders/stockSpider.py
+++ StockScrapyProject/StockScrapyProject/spiders/stockSpider.py
@@ -1,18 +1,18 @@
 import csv
 import logging
 import urllib.parse as urlParse
+import winsound
 from datetime import datetime
 from urllib.parse import parse_qs
 
-import winsound
-from pandas import DataFrame
 import PySimpleGUI as sg
 import scrapy
+from pandas import DataFrame
 from pydispatch import dispatcher
 from scrapy import signals
 from scrapy.exceptions import CloseSpider
-from ..items import StockSpider_items
 
+from ..items import StockSpider_items
 
 
 class StockSpider(scrapy.Spider):
@@ -56,10 +56,12 @@ class StockSpider(scrapy.Spider):
             logging.info("正常運算當中")
         self.info = (
             f"CSV總筆數:{self.total},匯入有效筆數:{self.ready_crawl}\n目前筆數:{self.current},確認存在股數:{self.exist}\n確認未存在股號數:{len(self.noExist)},待導入A類查尋筆數:{self.wait_url_A}")
-        self.not_mamual_cancel = sg.one_line_progress_meter('目前爬取進度',self.current,self.ready_crawl+self.wait_url_A_MAX,'Stock','運行時請勿點擊視窗，顯示沒有回應請勿關閉，為正常現象。\nElapsed Time 為已運行時間\nTime Remaining 為剩餘時間\nEstimated Total Time 為估計完成時間',no_titlebar=False,orientation='h')
-        if(not self.not_mamual_cancel and self.current < self.exist+self.wait_url_A):
-            sg.popup('已手動取消！')
-            raise CloseSpider("使用者取消！")
+        self.not_manual_cancel = sg.one_line_progress_meter('目前爬取進度',self.current,self.ready_crawl-1,'Stock','運行時請勿點擊視窗，顯示沒有回應請勿關閉，為正常現象。\nElapsed Time 為已運行時間\nTime Remaining 為剩餘時間\nEstimated Total Time 為估計完成時間',no_titlebar=False,orientation='h')
+        if(not self.not_manual_cancel and self.current < self.exist+self.wait_url_A):
+            Button = sg.popup_yes_no('是否取消？','取消爬取')
+            if(Button=='Yes'):
+                sg.popup('已手動取消！')
+                raise CloseSpider("使用者取消！")
         
         logging.info(self.info)
         print("\n未存在股號列表：")
@@ -68,11 +70,9 @@ class StockSpider(scrapy.Spider):
 
     def manual_Mode(self, CO_ID):  # 手動模式
         if((len(CO_ID) == 4) and CO_ID.isnumeric()):
-            self.exist += 1
+            self.ready_crawl = 1
             self.start_urls.append(
                 f'https://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID={CO_ID}&SYEAR={self.Year}&SSEASON={self.Season}&REPORT_ID=C')  # 帶入網址序列
-            self.start_urls.append(
-                f'https://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID={CO_ID}&SYEAR={self.Year}&SSEASON={self.Season}&REPORT_ID=A')  # 帶入網址序列
         else:
             logging.error('請輸入正確的四位數純數字股號')
             pass
@@ -87,7 +87,7 @@ class StockSpider(scrapy.Spider):
             filename = f'.\{dt_string} -財務報告- {self.Year} 年第 {self.Season} 季中未存在股號.csv'
             df.to_csv(filename, index=False)
             print(f'已匯出未存在的股號至{filename}')
-            sg.SystemTray.notify('爬取完成',f'已匯出未存在的股號至\n{filename}')
+            sg.SystemTray.notify('財務報告爬蟲',f'爬取完成！\n已匯出未存在的股號至\n{filename}')
         else:
             logging.info('無缺漏股號。')
 
@@ -103,12 +103,12 @@ class StockSpider(scrapy.Spider):
         self.Season = Season  # 帶入參數季度 -a Season 數字字串
         self.Mode = Mode  # 帶入爬蟲模式 -a Mode 文字字串，Auto與Manual模式
         if(Mode == 'Auto' or Mode == 'A'):
-            sg.SystemTray.notify('財務報告爬蟲－初始化', '以批次模式進行中...', display_duration_in_ms=300, fade_in_duration=.2)
+            sg.SystemTray.notify('財務報告爬蟲', '初始化 - 以批次模式進行中...', display_duration_in_ms=300, fade_in_duration=.2)
             logging.info(f'目前輸入的參數，年份：{Year}、季度{Season}、模式：{Mode}、CSV路徑:{CSV}')
             self.import_csv = CSV  # 匯入CSV之路徑 -a CSV 'Path'
             self.auto_Mode()
         elif (Mode == 'Manual' or Mode == 'M'):
-            sg.SystemTray.notify('財務報告爬蟲－初始化', '以單筆模式進行中...', display_duration_in_ms=300, fade_in_duration=.2)
+            sg.SystemTray.notify('財務報告爬蟲', '初始化 - 以單筆模式進行中...', display_duration_in_ms=300, fade_in_duration=.2)
             logging.info(f'目前輸入的參數，年份：{Year}、季度{Season}、模式：{Mode}、股號:{CO_ID}')
             self.manual_Mode(CO_ID)
         else:
@@ -123,20 +123,76 @@ class StockSpider(scrapy.Spider):
         except ValueError:
             return False
 
-    def get_From_Table(self, items, response, tables_ID, tables_ItemsName):  # 從表格當中獲取資料
+    def get_From_Table(self, items, response, tables_ID, tables_ItemsName,isTable1):  # 從表格當中獲取資料
         for datas in response.xpath('body/div[2]/div[3]'):
             for tableID in range(0, len(tables_ID)):  # 表一獲取資料
-                data = datas.xpath(
-                    f"//td[contains(text(),'{tables_ID[tableID]}')]/following-sibling::td[2]//text()").getall()
-                if(len(data)):
-                    data[0] = data[0].replace(',', '')
-                    if(self.is_Number(data[0])):
-                        items[tables_ItemsName[tableID]] = float(data[0])
+                if(isTable1):
+                    tables1_ID_Type2 = ['11000', '1110', '1120','1136', '1139', '25XX', '31101']
+                    tables1_ID_Type3 = ['111100', '112000', '1120','1136', '1139', '220000', '31100']
+                    tables1_ID_Type4 = ['111100', '112000', '1120','1136', '1139', '220000', '301010']
+                    data = datas.xpath(
+                    f"//td[text() = '{tables_ID[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    data2 = datas.xpath(
+                    f"//td[text() = '{tables1_ID_Type2[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    data3 = datas.xpath(
+                    f"//td[text() = '{tables1_ID_Type3[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    data4 = datas.xpath(
+                    f"//td[text() = '{tables1_ID_Type4[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    if(len(data)):
+                        data[0] = data[0].replace(',', '')
+                        if(self.is_Number(data[0])):
+                            items[tables_ItemsName[tableID]] = float(data[0])
+                            continue
+                        else:
+                            data[1] = data[1].replace(',', '')
+                            items[tables_ItemsName[tableID]] = -(float(data[1]))
+                            continue
+                    elif(len(data2)):
+                        data2[0] = data2[0].replace(',', '')
+                        if(self.is_Number(data2[0])):
+                            items[tables_ItemsName[tableID]] = float(data2[0])
+                            continue
+                        else:
+                            data2[1] = data2[1].replace(',', '')
+                            items[tables_ItemsName[tableID]] = -(float(data2[1]))
+                            continue
+                    elif(len(data3)):
+                        data3[0] = data3[0].replace(',', '')
+                        if(self.is_Number(data3[0])):
+                            items[tables_ItemsName[tableID]] = float(data3[0])
+                            continue
+                        else:
+                            data3[1] = data3[1].replace(',', '')
+                            items[tables_ItemsName[tableID]] = -(float(data3[1]))
+                            continue
                     else:
-                        data[1] = data[1].replace(',', '')
-                        items[tables_ItemsName[tableID]] = -(float(data[1]))
+                        items[tables_ItemsName[tableID]] = float(0.0)
                 else:
-                    items[tables_ItemsName[tableID]] = float(0.0)
+                    tables2_ID_Type2 = ['41000', '61000', '59000', '985000']
+                    data = datas.xpath(
+                    f"//td[text() = '{tables_ID[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    data2 = datas.xpath(
+                    f"//td[text() = '{tables2_ID_Type2[tableID]}']/following-sibling::td[2]//text()").getall() #td[2]使用代號，td[1]使用名稱定位
+                    if(len(data)):
+                        data[0] = data[0].replace(',', '')
+                        if(self.is_Number(data[0])):
+                            items[tables_ItemsName[tableID]] = float(data[0])
+                            continue
+                        else:
+                            data[1] = data[1].replace(',', '')
+                            items[tables_ItemsName[tableID]] = -(float(data[1]))
+                            continue
+                    elif(len(data2)):
+                        data2[0] = data2[0].replace(',', '')
+                        if(self.is_Number(data2[0])):
+                            items[tables_ItemsName[tableID]] = float(data2[0])
+                            continue
+                        else:
+                            data2[1] = data2[1].replace(',', '')
+                            items[tables_ItemsName[tableID]] = -(float(data2[1]))
+                            continue
+                    else:
+                        items[tables_ItemsName[tableID]] = float(0.0)
 
     def parse(self, response):  # 擷取開始
         _page_exist = True
@@ -154,6 +210,7 @@ class StockSpider(scrapy.Spider):
                 _page_exist = False
                 logging.error("該股A與C類皆無資料，記錄至未存在表中。")
                 self.noExist.append(str(company_Id[0]))
+                self.current += 1
                 self.wait_url_A -= 1
                 pass
             else:  # 試圖用回報A連結重新爬取
@@ -165,6 +222,7 @@ class StockSpider(scrapy.Spider):
                     f'https://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID={company_Id[0]}&SYEAR={self.Year}&SSEASON={self.Season}&REPORT_ID=A')
                 pass
         if(_page_exist):
+            self.current += 1
             items['DATA_TYPE'] = self.Type
             items['SUB_DATA_TYPE'] = '個別財務報告' if (
                 report_ID == 'A') else '合併財務報告'
@@ -175,13 +233,14 @@ class StockSpider(scrapy.Spider):
             items['SYear'] = self.Year
             items['SSeason'] = self.Season
             # 主要爬蟲區
-            tables1_ID = ['1100', '1110', '1120',
-                          '1136', '1139', '25XX', '3110']
+            tables1_ID = ['1100', '1110', '1120','1136', '1139', '25XX', '3110']
+            #tables1_ID = ['現金及約當現金', '透過損益按公允價值衡量之金融資產－流動', '透過其他綜合損益按公允價值衡量之金融資產－流動',
+            #              '按攤銷後成本衡量之金融資產－流動', '避險之金融資產－流動', '非流動負債合計', '普通股股本']
             tables1_ItemsName = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7']
+            #tables2_ID = ['營業收入合計', '營業利益（損失）', '營業外收入及支出合計', '稀釋每股盈餘合計']
             tables2_ID = ['4000', '6900', '7000', '9850']
             tables2_ItemsName = ['B1', 'B2', 'B3', 'B4']
-            self.get_From_Table(items, response, tables1_ID, tables1_ItemsName)
-            self.get_From_Table(items, response, tables2_ID, tables2_ItemsName)
+            self.get_From_Table(items, response, tables1_ID, tables1_ItemsName,True)
+            self.get_From_Table(items, response, tables2_ID, tables2_ItemsName,False)
             yield(items)
         self.print_info()
-        self.current += 1
