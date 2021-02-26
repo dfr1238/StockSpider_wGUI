3c954d17283996b4061d3c16581176eb0ea23e4a
dfr1238 <dfr1238@gmail.com>
2021-02-26 21:11:38 +0800

    移除測試檔案

--- url_parse_test.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse as urlParse
-from urllib.parse import parse_qs
-
-url ='https://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID=9188&SYEAR=2019&SSEASON=3&REPORT_ID=C'
-parsed = urlParse.urlparse(url)
-company_Id=parse_qs(parsed.query)['CO_ID'] #獲取網址股號
-report_ID=parse_qs(parsed.query)['REPORT_ID'] #獲取回報ID
-print(f"CO_ID: {company_Id[0]},RID: {report_ID[0]}")
